8b3e2f8466f3fd33e5659c68f79871160f57e632
Michillas <elrickimont@gmail.com>
2024-03-13 21:31:34 +0100

    PersonalityQuiz - Getting started with React-Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import {NextUIProvider} from '@nextui-org/react'
+
 import App from './App.jsx'
 import './index.css'
 
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <NextUIProvider>
+        <div className="dark">
+          <App />
+        </div>
+      </NextUIProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
