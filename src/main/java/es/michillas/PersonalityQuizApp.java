c9efaad876ee370b3baf4dd81087e5a3e2fbdf5d
Michillas <elrickimont@gmail.com>
2024-03-23 13:29:21 +0100

    PersonalityQuiz - Remove testing imports

--- src/main/java/es/michillas/PersonalityQuizApp.java
+++ src/main/java/es/michillas/PersonalityQuizApp.java
@@ -1,11 +1,7 @@
 package es.michillas;
 
-import es.michillas.models.Usuario;
-import es.michillas.services.UsuarioService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class PersonalityQuizApp {
