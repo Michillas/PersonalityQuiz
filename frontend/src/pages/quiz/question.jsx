4dcae0d70d96ca50c1a922741392dab1eee42aff
Michillas <elrickimont@gmail.com>
2024-03-24 00:16:40 +0100

    PersonalityQuiz - I kinda like what I see

--- frontend/src/pages/quiz/question.jsx
+++ frontend/src/pages/quiz/question.jsx
@@ -1,11 +1,29 @@
 /* eslint-disable react/prop-types */
-import {RadioGroup, Radio, Divider} from "@nextui-org/react";
+import { useState } from 'react';
+import { RadioGroup, Radio, Divider } from "@nextui-org/react";
 
 export default function Question(props) {
+    const [selectedValue, setSelectedValue] = useState(0);
 
-    const handleValueChange = {
-        
-    }
+    const handleValueChange = (value) => {
+        setSelectedValue(value);
+        switch (props.type) {
+            case "focus":
+                props.changeValue(props.setFocus, value);
+                break;
+            case "information":
+                props.changeValue(props.setInformation, value);
+                break;
+            case "decisions":
+                props.changeValue(props.setDecisions, value);
+                break;
+            case "outerlife":
+                props.changeValue(props.setOuterlife, value);
+                break;
+            default:
+                break;
+        }
+    };
 
     return (
         <>
@@ -17,21 +35,19 @@ export default function Question(props) {
                         color="secondary"
                         orientation="horizontal"
                         onValueChange={handleValueChange}
+                        value={selectedValue}
                     >
-                        <Radio color="success" classNames={{wrapper: ['h-20 w-20'], control: ['h-20 w-20']}} value="3"/>
-                        <Radio color="success" classNames={{wrapper: ['h-16 w-16'], control: ['h-16 w-16']}} value="2"/>
-                        <Radio color="success" classNames={{wrapper: ['h-12 w-12'], control: ['h-12 w-12']}} value="1"/>
-                        <Radio color="default" classNames={{wrapper: ['h-12 w-12'], control: ['h-12 w-12']}} value="0"/>
-                        <Radio color="secondary" classNames={{wrapper: ['h-12 w-12'], control: ['h-12 w-12']}}
-                               value="-1"/>
-                        <Radio color="secondary" classNames={{wrapper: ['h-16 w-16'], control: ['h-16 w-16']}}
-                               value="-2"/>
-                        <Radio color="secondary" classNames={{wrapper: ['h-20 w-20'], control: ['h-20 w-20']}}
-                               value="-3"/>
+                        <Radio color="success" classNames={{ wrapper: ['h-20 w-20'], control: ['h-20 w-20'] }} value="3" />
+                        <Radio color="success" classNames={{ wrapper: ['h-16 w-16'], control: ['h-16 w-16'] }} value="2" />
+                        <Radio color="success" classNames={{ wrapper: ['h-12 w-12'], control: ['h-12 w-12'] }} value="1" />
+                        <Radio color="default" classNames={{ wrapper: ['h-12 w-12'], control: ['h-12 w-12'] }} value="0" />
+                        <Radio color="secondary" classNames={{ wrapper: ['h-12 w-12'], control: ['h-12 w-12'] }} value="-1" />
+                        <Radio color="secondary" classNames={{ wrapper: ['h-16 w-16'], control: ['h-16 w-16'] }} value="-2" />
+                        <Radio color="secondary" classNames={{ wrapper: ['h-20 w-20'], control: ['h-20 w-20'] }} value="-3" />
                     </RadioGroup>
                     <div className="w-40 text-center">No estoy de acuerdo</div>
                 </div>
-                <Divider className="mt-12"/>
+                <Divider className="mt-12" />
             </div>
         </>
     )
--- frontend/src/pages/quiz/quiz.jsx
+++ frontend/src/pages/quiz/quiz.jsx
@@ -21,14 +21,31 @@ export default function Quiz() {
     };
 
     const [focus, setFocus] = useState(0);
-    const [information, setInformation] = useState(1);
-    const [decisions, setDecisions] = useState(1);
+    const [information, setInformation] = useState(0);
+    const [decisions, setDecisions] = useState(0);
     const [outerlife, setOuterlife] = useState(0);
 
+    const changeValue = (setState, value) => {
+        setState(prevState => {
+            // Parse prevState as a number to ensure correct arithmetic operation
+            const parsedPrevState = parseInt(prevState, 10);
+            // Parse value as a number to ensure correct arithmetic operation
+            const parsedValue = parseInt(value, 10);
+            // Perform arithmetic operation and return the new state
+            return parsedPrevState + parsedValue;
+        });
+    };
+
+    const [mbti, setMbti] = useState('');
+
     const handleSendQuiz = async () => {
+
+        calculateMbti()
+
         try {
             const usuarioData = {
                 username: name,
+                mbti: mbti,
                 focus: focus,
                 information: information,
                 decisions: decisions,
@@ -55,10 +72,27 @@ export default function Quiz() {
         }
     };
 
+    const calculateMbti = () => {
+        
+        const focusSum = focus;
+        const informationSum = information;
+        const decisionsSum = decisions;
+        const outerlifeSum = outerlife;
+    
+        const mbtiType = [
+            focusSum >= 0 ? 'I' : 'E',
+            informationSum >= 0 ? 'N' : 'S',
+            decisionsSum >= 0 ? 'F' : 'T',
+            outerlifeSum >= 0 ? 'P' : 'J'
+        ].join('');
+    
+        setMbti(mbtiType);
+    };
+
     return (
         <div className='min-h-[87.4vh]'>
             {questions.map((pregunta, index) => (
-                <Question key={index} title={pregunta.question} type={pregunta.type} setFocus={setFocus} setInformation={setInformation} setDecisions={setDecisions} setOuterlife={setOuterlife} />
+                <Question key={index} title={pregunta.question} type={pregunta.type} changeValue={changeValue} setFocus={setFocus} setInformation={setInformation} setDecisions={setDecisions} setOuterlife={setOuterlife} />
             ))}
             <div className="flex flex-col items-center mt-6 pb-12">
                 <Input id="nombre" type="text" variant="flat" label="Nombre" placeholder="Escribe tu nombre" className="max-w-64" value={name} onChange={(e) => setName(e.target.value)} />
