d7997ee4ae53d2982e2ca6da1a89ade9b0c2031d
Michillas <elrickimont@gmail.com>
2024-03-24 16:15:01 +0100

    PersonalityQuiz - Day 3

---
+++ frontend/src/pages/quiz/result.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+export default function Result(props) {
+    
+    return( 
+        <>
+            <div>Eres</div>
+            <div>{props.letters}</div>
+        </>
+    )
+}
