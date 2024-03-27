776d07af4df0a3788a9817baadf321e42bf2fce1
Michillas <elrickimont@gmail.com>
2024-03-27 16:57:11 +0100

    PersonalityQuiz - Little improvements

--- frontend/src/pages/quiz/question.jsx
+++ frontend/src/pages/quiz/question.jsx
@@ -26,29 +26,27 @@ export default function Question(props) {
     };
 
     return (
-        <>
-            <div className="flex flex-col items-center justify-center p-6">
-                <div className="text-center text-3xl font-semibold mt-2">{props.title}</div>
-                <div className="flex flex-row items-center justify-center gap-6 mt-6">
-                    <div className="w-40 text-center">Estoy de acuerdo</div>
-                    <RadioGroup
-                        color="secondary"
-                        orientation="horizontal"
-                        onValueChange={handleValueChange}
-                        value={selectedValue}
-                    >
-                        <Radio color="success" classNames={{ wrapper: ['lg:h-20 lg:w-20 sm:h-10 sm:w-10'], control: ['lg:h-20 lg:w-20 sm:h-10 sm:w-10'] }} value="3" />
-                        <Radio color="success" classNames={{ wrapper: ['lg:h-16 lg:w-16 sm:h-8 sm:w-8'], control: ['lg:h-16 lg:w-16 sm:h-8 sm:w-8'] }} value="2" />
-                        <Radio color="success" classNames={{ wrapper: ['lg:h-12 lg:w-12 sm:h-6 sm:w-6'], control: ['lg:h-12 lg:w-12 sm:h-6 sm:w-6'] }} value="1" />
-                        <Radio color="default" classNames={{ wrapper: ['lg:h-12 lg:w-12 sm:h-6 sm:w-6'], control: ['lg:h-12 lg:w-12 sm:h-6 sm:w-6'] }} value="0" />
-                        <Radio color="secondary" classNames={{ wrapper: ['lg:h-12 lg:w-12 sm:h-6 sm:w-6'], control: ['lg:h-12 lg:w-12 sm:h-6 sm:w-6'] }} value="-1" />
-                        <Radio color="secondary" classNames={{ wrapper: ['lg:h-16 lg:w-16 sm:h-8 sm:w-8'], control: ['lg:h-16 lg:w-16 sm:h-8 sm:w-8'] }} value="-2" />
-                        <Radio color="secondary" classNames={{ wrapper: ['lg:h-20 lg:w-20 sm:h-10 sm:w-10'], control: ['lg:h-20 lg:w-20 sm:h-10 sm:w-10'] }} value="-3" />
-                    </RadioGroup>
-                    <div className="w-40 text-center">No estoy de acuerdo</div>
-                </div>
-                <Divider className="mt-12" />
+        <div className="flex flex-col items-center justify-center p-6">
+            <div className="text-center text-3xl font-semibold mt-2">{props.title}</div>
+            <div className="flex flex-row items-center justify-center gap-6 mt-6">
+                <div className="w-40 text-center">Estoy de acuerdo</div>
+                <RadioGroup
+                    color="secondary"
+                    orientation="horizontal"
+                    onValueChange={handleValueChange}
+                    value={selectedValue}
+                >
+                    <Radio color="success" classNames={{ wrapper: ['lg:h-20 lg:w-20 sm:h-10 sm:w-10'], control: ['lg:h-20 lg:w-20 sm:h-10 sm:w-10'] }} value="3" />
+                    <Radio color="success" classNames={{ wrapper: ['lg:h-16 lg:w-16 sm:h-8 sm:w-8'], control: ['lg:h-16 lg:w-16 sm:h-8 sm:w-8'] }} value="2" />
+                    <Radio color="success" classNames={{ wrapper: ['lg:h-12 lg:w-12 sm:h-6 sm:w-6'], control: ['lg:h-12 lg:w-12 sm:h-6 sm:w-6'] }} value="1" />
+                    <Radio color="default" classNames={{ wrapper: ['lg:h-12 lg:w-12 sm:h-6 sm:w-6'], control: ['lg:h-12 lg:w-12 sm:h-6 sm:w-6'] }} value="0" />
+                    <Radio color="secondary" classNames={{ wrapper: ['lg:h-12 lg:w-12 sm:h-6 sm:w-6'], control: ['lg:h-12 lg:w-12 sm:h-6 sm:w-6'] }} value="-1" />
+                    <Radio color="secondary" classNames={{ wrapper: ['lg:h-16 lg:w-16 sm:h-8 sm:w-8'], control: ['lg:h-16 lg:w-16 sm:h-8 sm:w-8'] }} value="-2" />
+                    <Radio color="secondary" classNames={{ wrapper: ['lg:h-20 lg:w-20 sm:h-10 sm:w-10'], control: ['lg:h-20 lg:w-20 sm:h-10 sm:w-10'] }} value="-3" />
+                </RadioGroup>
+                <div className="w-40 text-center">No estoy de acuerdo</div>
             </div>
-        </>
+            <Divider className="mt-12" />
+        </div>
     )
 }
