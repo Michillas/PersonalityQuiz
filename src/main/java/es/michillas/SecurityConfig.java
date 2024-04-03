18f8be1ccb73e43de77c3f1d1d90db8ee4b3875b
Michillas <elrickimont@gmail.com>
2024-04-03 03:39:30 +0200

    PersonalityQuiz - SecurityConfig changes

--- src/main/java/es/michillas/SecurityConfig.java
+++ src/main/java/es/michillas/SecurityConfig.java
@@ -5,8 +5,41 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().permitAll()
+                );
+        // @formatter:on
+        return http.build();
+    }
+
+    // @formatter:off
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+    // @formatter:on
 
     @Bean
     public PasswordEncoder passwordEncoder() {
