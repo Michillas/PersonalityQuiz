435b502dc88289b6b4f4e12baa605a72e819805e
Michillas <elrickimont@gmail.com>
2024-04-04 01:36:19 +0200

    PersonalityQuiz - Removed deprecated method in SecurityConfig

--- src/main/java/es/michillas/SecurityConfig.java
+++ src/main/java/es/michillas/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -20,24 +21,20 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // @formatter:off
         http.authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll()).csrf().disable();
-        ;
-        // @formatter:on
+        
         return http.build();
     }
 
-    // @formatter:off
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+            .username("user")
+            .password(passwordEncoder().encode("password"))  // Use a password encoder
+            .roles("USER")
+            .build();
         return new InMemoryUserDetailsManager(user);
     }
-    // @formatter:on
 
     @Bean
     public PasswordEncoder passwordEncoder() {
