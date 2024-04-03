777f432967cd163d75f51f87466ea6530180f097
Michillas <elrickimont@gmail.com>
2024-04-03 05:54:29 +0200

    PersonalityQuiz - Now admin dashboard asks for authentication

--- frontend/src/pages/mvc/admin.jsx
+++ frontend/src/pages/mvc/admin.jsx
@@ -1,10 +1,19 @@
 import CrudTable from './CrudTable'
 
+import { useAuth } from "../../components/auth/AuthContext";
+
 export default function Admin() {
+
+  const { isLoggedIn } = useAuth();
 
   return (
     <div className='min-h-[90.8vh] p-[6.9vh]'>
+      {isLoggedIn && 
       <CrudTable/>
+      }
+      {!isLoggedIn && 
+      <h1 className="text-center text-large">No tienes permisos para ver esta página</h1>
+      }
     </div>
   );
 }
