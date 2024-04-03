fa9d55d6969683647020f649ff567b06cdf3e429
Michillas <elrickimont@gmail.com>
2024-04-03 22:37:36 +0200

    PersonalityQuiz - Modified the imports to try if vercel likes them more

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import Header from "./Header.jsx";
+import Header from "./Header";
 
 import { Routes, Route } from "react-router-dom";
-import Index from "./pages/index/Index.jsx";
-import Mbti from "./pages/mbti/Mbti.jsx";
-import Quiz from "./pages/quiz/Quiz.jsx";
-import Result from "./pages/quiz/Result.jsx";
-import Admin from "./pages/mvc/Admin.jsx";
-import Login from "./pages/login/Login.jsx";
-import Register from "./pages/login/Register.jsx";
-import NotFound from "./pages/notfound/NotFound.jsx";
+import Index from "./pages/index/Index";
+import Mbti from "./pages/mbti/Mbti";
+import Quiz from "./pages/quiz/Quiz";
+import Result from "./pages/quiz/Result";
+import Admin from "./pages/mvc/Admin";
+import Login from "./pages/login/Login";
+import Register from "./pages/login/Register";
+import NotFound from "./pages/notfound/NotFound";
 
-import { AuthProvider } from "./components/auth/AuthContext.jsx";
+import { AuthProvider } from "./components/auth/AuthContext";
 
 function App() {
   return (
