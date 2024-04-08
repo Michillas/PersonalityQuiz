b1229641a0fe900e8f37180980a7d4d8208e9699
Michillas <elrickimont@gmail.com>
2024-04-08 20:12:29 +0200

    PersonalityQuiz - Removed test error mapping

--- src/main/java/es/michillas/PersonalityQuizApp.java
+++ src/main/java/es/michillas/PersonalityQuizApp.java
@@ -2,20 +2,10 @@ package es.michillas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class PersonalityQuizApp {
     public static void main(String[] args) {
         SpringApplication.run(PersonalityQuizApp.class, args);
-    }
-
-    @RestController
-    @RequestMapping("/")
-    static class Controller {
-        @GetMapping("/error")
-        String handleError() {
-            return "Error";
-        }
     }
 }
