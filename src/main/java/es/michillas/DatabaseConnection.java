cf17edba5c34593977f75c914f7f4afe7d255150
Michillas <elrickimont@gmail.com>
2024-04-09 11:09:04 +0200

    PersonalityQuiz - DB password saved in enviroment variable

--- src/main/java/es/michillas/DatabaseConnection.java
+++ src/main/java/es/michillas/DatabaseConnection.java
@@ -1,22 +1,39 @@
 package es.michillas;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    String url = "jdbc:mysql://localhost:3306/personalityquiz?allowPublicKeyRetrieval=true";
-    String user = "root";
-    String pass = "";
+    String url = "jdbc:mysql://avnadmin:" + loadPasswordFromEnv() + "@mysql-personalityquiz-personalityquiz.a.aivencloud.com:15210/personalityquiz?ssl-mode=REQUIRED";
+    String user = "avnadmin";
+    String pass = loadPasswordFromEnv();
 
     Connection connection;
 
     public Connection connect(){
         try {
-            connection = DriverManager.getConnection(url, user,pass);
+            connection = DriverManager.getConnection(url, user, pass);
         } catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
         return connection;
+    }
+
+    private String loadPasswordFromEnv() {
+        try (BufferedReader br = new BufferedReader(new FileReader("./.env"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("DB_PASSWORD")) {
+                    return line.split("=")[1];
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null; // Handle if DB_PASSWORD is not found
     }
 }
